2fa90ca3e1892c0396b24570fb633e2741793336
benaclejames <benaclejames@gmail.com>
2020-10-12 03:35:36 +0100

    Big optimization fix

--- EyeTrack/MainMod.cs
+++ EyeTrack/MainMod.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
+using System.Threading.Tasks;
+using Il2CppSystem;
 using MelonLoader;
+using UnhollowerBaseLib;
 using VRC.SDK3.Avatars.ScriptableObjects;
 using UnityEngine;
 
@@ -14,6 +17,8 @@ namespace EyeTrack
         {
             _tracker.Start();
             MelonCoroutines.Start(UpdatePriority());
+            MelonCoroutines.Start(UpdateParamEnums());
+            MelonCoroutines.Start(UpdateParams());
             MelonLogger.Log("SRanipal SDK Started. Eye Tracking Active");
         }
 
@@ -30,15 +35,40 @@ namespace EyeTrack
 
         private static int GetParamIndex(string paramName)
         {
-            var parameters = VRCPlayer
-                .field_Internal_Static_VRCPlayer_0
-                .prop_VRCAvatarManager_0.prop_VRCAvatarDescriptor_0.expressionParameters.parameters;
+            VRCExpressionParameters.Parameter[] parameters = new VRCExpressionParameters.Parameter[0];
 
+            if (VRCPlayer
+                .field_Internal_Static_VRCPlayer_0 != null)
+            {
+                if (VRCPlayer
+                    .field_Internal_Static_VRCPlayer_0
+                    .prop_VRCAvatarManager_0 != null)
+                    if (VRCPlayer
+                        .field_Internal_Static_VRCPlayer_0
+                        .prop_VRCAvatarManager_0.prop_VRCAvatarDescriptor_0 != null)
+                        if (VRCPlayer
+                            .field_Internal_Static_VRCPlayer_0
+                            .prop_VRCAvatarManager_0.prop_VRCAvatarDescriptor_0.expressionParameters != null)
+                            if (VRCPlayer
+                                    .field_Internal_Static_VRCPlayer_0
+                                    .prop_VRCAvatarManager_0.prop_VRCAvatarDescriptor_0.expressionParameters
+                                    .parameters !=
+                                null)
+                                parameters = VRCPlayer
+                                    .field_Internal_Static_VRCPlayer_0
+                                    .prop_VRCAvatarManager_0.prop_VRCAvatarDescriptor_0.expressionParameters
+                                    .parameters;
+
+            }
+            else
+                return -1;
+            
             var index = -1;
-
             for (var i = 0; i < parameters.Length; i++)
             {
                 VRCExpressionParameters.Parameter param = parameters[i];
+                if (param.name == null)
+                    return -1;
                 if (param.name == paramName)
                 {
                     index = i;
@@ -46,22 +76,6 @@ namespace EyeTrack
             }
 
             return index;
-        }
-
-        private AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique GetParamEnum(string paramName)
-        {
-            if (VRCPlayer.field_Internal_Static_VRCPlayer_0.prop_VRCAvatarManager_0 == null || VRCPlayer
-                .field_Internal_Static_VRCPlayer_0
-                .prop_VRCAvatarManager_0.prop_VRCAvatarDescriptor_0 == null)
-                return AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique.None;
-
-            int paramIndex = GetParamIndex(paramName);
-            if (paramIndex == -1)
-                return AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique.None;
-
-            var foundParam =
-                (AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique) paramIndex;
-            return foundParam;
         }
 
         private void SetPriority(AvatarPlayableController controller, bool priority, string paramName)
@@ -105,41 +119,83 @@ namespace EyeTrack
         }
 
         void SetParameter(AvatarAnimParamController controller,
-            AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique param, float value)
+            param param, float value)
         {
-            if (controller.field_Private_AvatarPlayableController_0 == null ||
-                param == AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique.None)
+            if (controller == null || controller.field_Private_AvatarPlayableController_0 == null ||
+                param.paramEnum == AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique.None)
                 return;
             controller.field_Private_AvatarPlayableController_0
-                .Method_Public_Void_EnumNPublicSealedvaStNoSt18StStStStStUnique_Single_0(param, value);
+                .Method_Public_Void_EnumNPublicSealedvaStNoSt18StStStStStUnique_Single_0(param.paramEnum, value);
         }
 
-        public override void OnUpdate()
+        IEnumerator UpdateParams()
         {
-            if (VRCPlayer.field_Internal_Static_VRCPlayer_0 == null || VRCPlayer.field_Internal_Static_VRCPlayer_0
-                .field_Private_VRC_AnimationController_0 == null)
-                return;
+            for (;;)
+            {
+                AvatarAnimParamController controller = null;
+                if (VRCPlayer.field_Internal_Static_VRCPlayer_0 != null)
+                {
+                    if (VRCPlayer.field_Internal_Static_VRCPlayer_0
+                        .field_Private_VRC_AnimationController_0 != null)
+                        if (VRCPlayer.field_Internal_Static_VRCPlayer_0
+                            .field_Private_VRC_AnimationController_0
+                            .field_Private_AvatarAnimParamController_0)
+                            controller = VRCPlayer.field_Internal_Static_VRCPlayer_0
+                                .field_Private_VRC_AnimationController_0.field_Private_AvatarAnimParamController_0;
+                }
+                else
+                    yield return new WaitForSeconds(2);
+                if (controller == null)
+                    yield return new WaitForSeconds(2);
+                
+                SetParameter(controller, leftEyeLid, _tracker.LeftOpenness);
+                SetParameter(controller, rightEyeLid, _tracker.RightOpenness);
+                SetParameter(controller, eyesWiden, _tracker.CombinedWiden);
+                SetParameter(controller, eyesX, _tracker.GazeDirectionCombinedLocal.x);
+                SetParameter(controller, eyesY, _tracker.GazeDirectionCombinedLocal.y);
 
-            AvatarAnimParamController controller = VRCPlayer.field_Internal_Static_VRCPlayer_0
-                .field_Private_VRC_AnimationController_0
-                .field_Private_AvatarAnimParamController_0;
+                float normalizedFloat = (_tracker.Diameter / _tracker.MinOpen) / (_tracker.MaxOpen - _tracker.MinOpen);
+                normalizedFloat = Mathf.Clamp(normalizedFloat, 0, 1);
+                SetParameter(controller, eyesDilation, normalizedFloat);
+                
+                yield return new WaitForFixedUpdate();
+            }
+        }
 
-            var eyesX = GetParamEnum("EyesX");
-            var eyesY = GetParamEnum("EyesY");
-            var leftEyeLid = GetParamEnum("LeftEyeLid");
-            var rightEyeLid = GetParamEnum("RightEyeLid");
-            var eyesWiden = GetParamEnum("EyesWiden");
-            var eyesDilation = GetParamEnum("EyesDilation");
+        struct param
+        {
+            public AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique paramEnum;
+            public int paramIndex;
+        }
+        
+        private param eyesX,
+            eyesY,
+            leftEyeLid,
+            rightEyeLid,
+            eyesWiden,
+            eyesDilation;
 
-            SetParameter(controller, leftEyeLid, _tracker.LeftOpenness);
-            SetParameter(controller, rightEyeLid, _tracker.RightOpenness);
-            SetParameter(controller, eyesWiden, _tracker.CombinedWiden);
-            SetParameter(controller, eyesX, _tracker.GazeDirectionCombinedLocal.x);
-            SetParameter(controller, eyesY, _tracker.GazeDirectionCombinedLocal.y);
-
-            float normalizedFloat = (_tracker.Diameter / _tracker.MinOpen) / (_tracker.MaxOpen - _tracker.MinOpen);
-            normalizedFloat = Mathf.Clamp(normalizedFloat, 0, 1);
-            SetParameter(controller, eyesDilation, normalizedFloat);
+        param GetParam(string name)
+        {
+            var paramIndex = GetParamIndex(name);
+            return new param
+            {
+                paramIndex = paramIndex,
+                paramEnum = paramIndex != -1 ? (AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique) paramIndex : AvatarPlayableController.EnumNPublicSealedvaStNoSt18StStStStStUnique.None
+            };
+        }
+        
+        IEnumerator UpdateParamEnums()
+        {
+            for (;;) {
+                eyesX = GetParam("EyesX");
+                eyesY = GetParam("EyesY");
+                leftEyeLid = GetParam("LeftEyeLid");
+                rightEyeLid = GetParam("RightEyeLid");
+                eyesWiden = GetParam("EyesWiden");
+                eyesDilation = GetParam("EyesDilation");
+                yield return new WaitForSeconds(3f);
+            }
         }
     }
 }
