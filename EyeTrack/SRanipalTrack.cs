5c277c0bdb26f71d110566968490d7f4b8de0e84
eslafur <eslafur@gmail.com>
2020-12-27 16:18:14 +0100

    Use System Thread instead of MelonCoroutines

--- EyeTrack/SRanipalTrack.cs
+++ EyeTrack/SRanipalTrack.cs
@@ -5,6 +5,7 @@ using MelonLoader;
 using UnityEngine;
 using ViveSR.anipal;
 using ViveSR.anipal.Eye;
+using System.Threading;
 
 public class SRanipalTrack
 {
@@ -16,6 +17,7 @@ public class SRanipalTrack
 
     public float MaxOpen;
     public float MinOpen = 999;
+    private Thread updateThread;
 
     public Dictionary<string, float> SRanipalData = new Dictionary<string, float>
     {
@@ -40,74 +42,95 @@ public class SRanipalTrack
         _framework.EnableEyeVersion = SRanipal_Eye_Framework.SupportedEyeVersion.version1;
         _framework.StartFramework();
         SRanipal_API.Initial(SRanipal_Eye_v2.ANIPAL_TYPE_EYE_V2, IntPtr.Zero);
-        MelonCoroutines.Start(Update());
+        updateThread = new Thread(Update);
+        updateThread.Start();
     }
 
     public void Stop()
     {
-        MelonCoroutines.Stop(Update());
+        updateThread.Abort();
         _framework.StopFramework();
     }
 
-    private IEnumerator Update()
+    private void Update()
     {
-        for (;;)
+        //Target update rate: 1/5th tick rate.
+        //on my system: 11ms * 5 = 55ms = ~18hz
+        //leaves more cpu to the game and made the tracking feel less flickery
+        int fixedDeltaTimeMillis = Mathf.FloorToInt(Time.fixedDeltaTime * 1000) * 5;
+        MelonLogger.Log("Target Update Rate:" + fixedDeltaTimeMillis + "ms");
+
+        for (; ; )
         {
-            if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
-                SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT)
-                yield return new WaitForSeconds(0.05f);
+            DateTime start = DateTime.Now;
 
-            SRanipal_Eye_API.GetEyeData_v2(ref _latestEyeData);
-            var CombinedEyeReverse = Vector3.Scale(
-                _latestEyeData.verbose_data.combined.eye_data.gaze_direction_normalized,
-                new Vector3(-1, 1, 1));
-            var LeftEyeReverse = Vector3.Scale(_latestEyeData.verbose_data.left.gaze_direction_normalized,
-                new Vector3(-1, 1, 1));
-            var RightEyeReverse = Vector3.Scale(_latestEyeData.verbose_data.right.gaze_direction_normalized,
-                new Vector3(-1, 1, 1));
-
-            if (CombinedEyeReverse != new Vector3(1.0f, -1.0f, -1.0f))
+            if (!(SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
+                SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT))
             {
-                SRanipalData["EyesX"] = CombinedEyeReverse.x;
-                SRanipalData["EyesY"] = CombinedEyeReverse.y;
+                SRanipal_Eye_API.GetEyeData_v2(ref _latestEyeData);
+                var CombinedEyeReverse = Vector3.Scale(
+                    _latestEyeData.verbose_data.combined.eye_data.gaze_direction_normalized,
+                    new Vector3(-1, 1, 1));
+                var LeftEyeReverse = Vector3.Scale(_latestEyeData.verbose_data.left.gaze_direction_normalized,
+                    new Vector3(-1, 1, 1));
+                var RightEyeReverse = Vector3.Scale(_latestEyeData.verbose_data.right.gaze_direction_normalized,
+                    new Vector3(-1, 1, 1));
 
-                SRanipalData["LeftEyeX"] = LeftEyeReverse.x;
-                SRanipalData["LeftEyeY"] = LeftEyeReverse.y;
+                if (CombinedEyeReverse != new Vector3(1.0f, -1.0f, -1.0f))
+                {
+                    SRanipalData["EyesX"] = CombinedEyeReverse.x;
+                    SRanipalData["EyesY"] = CombinedEyeReverse.y;
 
-                SRanipalData["RightEyeX"] = RightEyeReverse.x;
-                SRanipalData["RightEyeY"] = RightEyeReverse.y;
+                    SRanipalData["LeftEyeX"] = LeftEyeReverse.x;
+                    SRanipalData["LeftEyeY"] = LeftEyeReverse.y;
+
+                    SRanipalData["RightEyeX"] = RightEyeReverse.x;
+                    SRanipalData["RightEyeY"] = RightEyeReverse.y;
+                }
+
+                SRanipalData["LeftEyeLid"] = _latestEyeData.verbose_data.left.eye_openness;
+                SRanipalData["RightEyeLid"] = _latestEyeData.verbose_data.right.eye_openness;
+
+                if (_latestEyeData.verbose_data.right.GetValidity(SingleEyeDataValidity
+                    .SINGLE_EYE_DATA_PUPIL_DIAMETER_VALIDITY))
+                {
+                    CurrentDiameter = _latestEyeData.verbose_data.right.pupil_diameter_mm;
+                    if (_latestEyeData.verbose_data.right.eye_openness >= 1f)
+                        UpdateMinMaxDilation(_latestEyeData.verbose_data.right.pupil_diameter_mm);
+                }
+                else if (_latestEyeData.verbose_data.left.GetValidity(SingleEyeDataValidity
+                    .SINGLE_EYE_DATA_PUPIL_DIAMETER_VALIDITY))
+                {
+                    CurrentDiameter = _latestEyeData.verbose_data.left.pupil_diameter_mm;
+                    if (_latestEyeData.verbose_data.left.eye_openness >= 1f)
+                        UpdateMinMaxDilation(_latestEyeData.verbose_data.left.pupil_diameter_mm);
+                }
+
+                var normalizedFloat = CurrentDiameter / MinOpen / (MaxOpen - MinOpen);
+                SRanipalData["EyesDilation"] = Mathf.Clamp(normalizedFloat, 0, 1);
+
+                SRanipalData["EyesWiden"] = _latestEyeData.expression_data.left.eye_wide >
+                                            _latestEyeData.expression_data.right.eye_wide
+                    ? _latestEyeData.expression_data.left.eye_wide
+                    : _latestEyeData.expression_data.right.eye_wide;
+
+                SRanipalData["LeftEyeWiden"] = _latestEyeData.expression_data.left.eye_wide;
+                SRanipalData["RightEyeWiden"] = _latestEyeData.expression_data.right.eye_wide;
             }
 
-            SRanipalData["LeftEyeLid"] = _latestEyeData.verbose_data.left.eye_openness;
-            SRanipalData["RightEyeLid"] = _latestEyeData.verbose_data.right.eye_openness;
+            //stabelize update rate by waiting rest of the target time.
+            //if target is not met, half the update rate.
+            //do not wait if half speed is not met.
+            TimeSpan delta = DateTime.Now - start;
 
-            if (_latestEyeData.verbose_data.right.GetValidity(SingleEyeDataValidity
-                .SINGLE_EYE_DATA_PUPIL_DIAMETER_VALIDITY))
+            if (delta.TotalMilliseconds < fixedDeltaTimeMillis)
             {
-                CurrentDiameter = _latestEyeData.verbose_data.right.pupil_diameter_mm;
-                if (_latestEyeData.verbose_data.right.eye_openness >= 1f)
-                    UpdateMinMaxDilation(_latestEyeData.verbose_data.right.pupil_diameter_mm);
+                Thread.Sleep((int)(fixedDeltaTimeMillis - delta.TotalMilliseconds));
             }
-            else if (_latestEyeData.verbose_data.left.GetValidity(SingleEyeDataValidity
-                .SINGLE_EYE_DATA_PUPIL_DIAMETER_VALIDITY))
+            else if (delta.TotalMilliseconds < fixedDeltaTimeMillis * 2)
             {
-                CurrentDiameter = _latestEyeData.verbose_data.left.pupil_diameter_mm;
-                if (_latestEyeData.verbose_data.left.eye_openness >= 1f)
-                    UpdateMinMaxDilation(_latestEyeData.verbose_data.left.pupil_diameter_mm);
+                Thread.Sleep((int)((fixedDeltaTimeMillis * 2) - delta.TotalMilliseconds));
             }
-
-            var normalizedFloat = CurrentDiameter / MinOpen / (MaxOpen - MinOpen);
-            SRanipalData["EyesDilation"] = Mathf.Clamp(normalizedFloat, 0, 1);
-
-            SRanipalData["EyesWiden"] = _latestEyeData.expression_data.left.eye_wide >
-                                        _latestEyeData.expression_data.right.eye_wide
-                ? _latestEyeData.expression_data.left.eye_wide
-                : _latestEyeData.expression_data.right.eye_wide;
-
-            SRanipalData["LeftEyeWiden"] = _latestEyeData.expression_data.left.eye_wide;
-            SRanipalData["RightEyeWiden"] = _latestEyeData.expression_data.right.eye_wide;
-
-            yield return new WaitForSeconds(0.005f);
         }
     }
 
