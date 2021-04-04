b356d3979409313170244655d6f671ddc7b30b2c
Ben <benaclejames@gmail.com>
2021-04-04 07:22:27 +0100

    Fix issue where lip params would update regardless of if tracking data was received for them, resetting them to 0 at the same time. This may be changed in the future depending on feedback or if a user selects the option in the menu

--- VRCEyeTracking/SRParam/SRanipalLipParameter.cs
+++ VRCEyeTracking/SRParam/SRanipalLipParameter.cs
@@ -9,16 +9,17 @@ namespace VRCEyeTracking.SRParam
 {
     public class SRanipalLipParameter : FloatBaseParam, ISRanipalParam
     {
-        private readonly Func<Dictionary<LipShape_v2, float>, float> _getSRanipalParam;
+        private readonly Func<Dictionary<LipShape_v2, float>, float?> _getSRanipalParam;
 
-        public SRanipalLipParameter(Func<Dictionary<LipShape_v2, float>, float> getValueFunc, string paramName,
+        public SRanipalLipParameter(Func<Dictionary<LipShape_v2, float>, float?> getValueFunc, string paramName,
             bool prioritised = false)
             : base(paramName, prioritised) => _getSRanipalParam = getValueFunc;
 
         public void RefreshParam(EyeData_v2? eyeData, Dictionary<LipShape_v2, float> lipData = null)
         {
             if (lipData == null) return;
-            ParamValue = _getSRanipalParam.Invoke(lipData);
+            var newParamValue = _getSRanipalParam.Invoke(lipData);
+            if (newParamValue.HasValue) ParamValue = newParamValue.Value;
         }
 
         void ISRanipalParam.ResetParam() => ResetParams();
