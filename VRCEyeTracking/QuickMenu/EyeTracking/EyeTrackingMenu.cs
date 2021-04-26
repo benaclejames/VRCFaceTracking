962693867ef6965da9aa302365919f3c5cae7388
Ben <benaclejames@gmail.com>
2021-04-26 04:40:57 +0100

    Fixed UI XY indicators resetting on tracking loss

--- VRCEyeTracking/QuickMenu/EyeTracking/EyeTrackingMenu.cs
+++ VRCEyeTracking/QuickMenu/EyeTracking/EyeTrackingMenu.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using MelonLoader;
 using UnityEngine;
 using UnityEngine.UI;
 using ViveSR.anipal.Eye;
@@ -30,17 +29,24 @@ namespace VRCEyeTracking.QuickMenu.EyeTracking
 
         public void UpdateEyeTrack(EyeData_v2 eyeData)
         {
-            UpdateXY(Vector3.Scale(
-                eyeData.verbose_data.left.gaze_direction_normalized,
-                new Vector3(-1, 1, 1)), Vector3.Scale(
-                eyeData.verbose_data.right.gaze_direction_normalized,
-                new Vector3(-1, 1, 1)));
+            Vector3? leftGazeDir = null, rightGazeDir = null;
+            if (eyeData.verbose_data.right.GetValidity(SingleEyeDataValidity.SINGLE_EYE_DATA_GAZE_DIRECTION_VALIDITY))
+                rightGazeDir = Vector3.Scale(
+                    eyeData.verbose_data.right.gaze_direction_normalized,
+                    new Vector3(-1, 1, 1));
+            
+            if (eyeData.verbose_data.left.GetValidity(SingleEyeDataValidity.SINGLE_EYE_DATA_GAZE_DIRECTION_VALIDITY))
+                leftGazeDir = Vector3.Scale(
+                    eyeData.verbose_data.left.gaze_direction_normalized,
+                    new Vector3(-1, 1, 1));
+                
+            UpdateXY(leftGazeDir, rightGazeDir);
         }
 
-        private void UpdateXY(Vector2 leftEye, Vector2 rightEye)
+        private void UpdateXY(Vector2? leftEye, Vector2? rightEye)
         {
-            _leftEyeVisualizer.ReceiveXY(leftEye.x, leftEye.y);
-            _rightEyeVisualizer.ReceiveXY(rightEye.x, rightEye.y);
+            if (leftEye.HasValue) _leftEyeVisualizer.ReceiveXY(leftEye.Value.x, leftEye.Value.y);
+            if (rightEye.HasValue) _rightEyeVisualizer.ReceiveXY(rightEye.Value.x, rightEye.Value.y);
         }
     }
 }
