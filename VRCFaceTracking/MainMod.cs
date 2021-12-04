57326b75741c8556bc40c51122a247e80a99f536
Adjerry91 <adjerry91@yahoo.com>
2021-12-03 18:46:22 -0600

    Custom Setup
    Add F6 CheckRuntimeSanity
    Change output path for VRChat

--- VRCFaceTracking/MainMod.cs
+++ VRCFaceTracking/MainMod.cs
@@ -48,6 +48,9 @@ namespace VRCFaceTracking
 
         public override void OnUpdate()
         {
+            if (Input.GetKeyDown(KeyCode.F6))
+                MelonCoroutines.Start(UnifiedLibManager.CheckRuntimeSanity());  //Restart SRanipleRuntime with F6
+            
             if (!UnifiedLibManager.ShouldThread) 
                 UnifiedLibManager.Update();
 
