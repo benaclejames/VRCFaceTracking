a9b95a83a6a0af53b8829eac3fd95ddda5424b63
AeroScripts <9335832+AeroScripts@users.noreply.github.com>
2022-01-18 10:08:19 -0500

    Implement PositiveNgativeAveragedShape and use it to implement combined left/right parameters
    Many people seem to be using only 1 side of the combined params to power both sides, which isnt ideal

--- VRCFaceTracking/Params/Lip/LipShapeConversion.cs
+++ VRCFaceTracking/Params/Lip/LipShapeConversion.cs
@@ -3,24 +3,66 @@ using ViveSR.anipal.Lip;
 
 namespace VRCFaceTracking.Params.Lip
 {
-    public class PositiveNegativeShape
+
+    public interface ICombinedShape
+    {
+        float GetBlendedLipShape(Dictionary<LipShape_v2, float> inputMap);
+    }
+
+    public class PositiveNegativeShape : ICombinedShape
     {
         private readonly LipShape_v2 _positiveShape, _negativeShape;
         private float _positiveCache, _negativeCache;
-        private bool _steps;
         
-        public PositiveNegativeShape(LipShape_v2 positiveShape, LipShape_v2 negativeShape, bool steps = false)
+        public PositiveNegativeShape(LipShape_v2 positiveShape, LipShape_v2 negativeShape)
         {
             _positiveShape = positiveShape;
             _negativeShape = negativeShape;
-            _steps = steps;
         }
 
         public float GetBlendedLipShape(Dictionary<LipShape_v2, float> inputMap)
         {
             if (inputMap.TryGetValue(_positiveShape, out var positiveResult)) _positiveCache = positiveResult;
             if (inputMap.TryGetValue(_negativeShape, out var negativeResult)) _negativeCache = negativeResult * -1;
-            return _steps ? (_positiveCache - _negativeCache) - 1 : _positiveCache + _negativeCache;
+            return _positiveCache + _negativeCache;
         }
     }
+
+    public class PositiveNegativeAveragedShape : ICombinedShape
+    {
+        private readonly LipShape_v2[] _positiveShapes, _negativeShapes;
+        private float[] _positiveCache, _negativeCache;
+        private float _positiveCount, _negativeCount;
+
+        public PositiveNegativeAveragedShape(LipShape_v2[] positiveShapes, LipShape_v2[] negativeShapes)
+        {
+            _positiveShapes = positiveShapes;
+            _negativeShapes = negativeShapes;
+            _positiveCache = new float[positiveShapes.Length];
+            _negativeCache = new float[negativeShapes.Length];
+            _positiveCount = (float)positiveShapes.Length;
+            _negativeCount = (float)negativeShapes.Length;
+        }
+
+        public float GetBlendedLipShape(Dictionary<LipShape_v2, float> inputMap)
+        {
+
+            float positive = 0;
+            float negative = 0;
+            for (int i = 0; i < _positiveCount; i++) {
+                if (inputMap.TryGetValue(_positiveShapes[i], out var positiveResult))
+                    _positiveCache[i] = positiveResult;
+                positive += _positiveCache[i];
+            }
+
+            for (int i = 0; i < _negativeCount; i++) {
+                if (inputMap.TryGetValue(_negativeShapes[i], out var negativeResult))
+                    _negativeCache[i] = negativeResult * -1;
+                negative += _negativeCache[i];
+            }
+
+            return (positive / _positiveCount) + (negative / _negativeCount);
+        }
+    }
+
 }
