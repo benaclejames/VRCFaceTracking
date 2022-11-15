95da139104ccfc5d994e572bfbbee3a0bed21384
regzo2 <regzo252@gmail.com>
2022-11-14 20:12:32 -0500

    Overhaul Unified Expressions to use VRCFT proposal parameters.

--- VRCFaceTracking/Params/Lip/LipShapes.cs
+++ VRCFaceTracking/Params/Lip/LipShapes.cs
@@ -3,88 +3,125 @@
     // TODO: Create expressions map to relate legacy shapes to new ones
     public enum UnifiedExpression
     {
-        JawRight, // +JawX
-        JawLeft, // -JawX
-        JawForward,
-        JawOpen,
-        MouthApeShape,
-        MouthUpperRight, // +MouthUpper
-        MouthUpperLeft, // -MouthUpper
-        MouthLowerRight, // +MouthLower
-        MouthLowerLeft, // -MouthLower
-        MouthUpperOverturn,
-        MouthLowerOverturn,
-        MouthPout,
-        MouthSmileRight, // +SmileSadRight
-        MouthSmileLeft, // +SmileSadLeft
-        MouthSadRight, // -SmileSadRight
-        MouthSadLeft, // -SmileSadLeft
-        CheekPuffRight,
-        CheekPuffLeft,
-        CheekSuck,
-        MouthUpperUpRight,
-        MouthUpperUpLeft,
-        MouthLowerDownRight,
-        MouthLowerDownLeft,
-        MouthUpperInside,
-        MouthLowerInside,
-        MouthLowerOverlay,
-        TongueLongStep1,
-        TongueLongStep2,
-        TongueDown, // -TongueY
-        TongueUp, // +TongueY
-        TongueRight, // +TongueX
-        TongueLeft, // -TongueX
-        TongueRoll,
-        TongueUpLeftMorph,
-        TongueUpRightMorph,
-        TongueDownLeftMorph,
-        TongueDownRightMorph,
-        SRanipalMax,
+        #region Eye Gaze Expressions
 
-        //Expanded Face Tracking Parameters
-        BrowsInnerUp,
-        BrowInnerUpLeft,
-        BrowInnerUpRight,
-        BrowsOuterUp,
-        BrowOuterUpLeft,
-        BrowOuterUpRight,
-        BrowsDown,
-        BrowDownLeft,
-        BrowDownRight,
-        EyesSquint,
+        // These are currently unused in the Lips expressions, and used in Eye Expressions.
+
+        // EyeLookOutRight,
+        // EyeLookInRight,
+        // EyeLookUpRight,
+        // EyeLookDownRight,
+        // EyeLookOutLeft,
+        // EyeLookInLeft,
+        // EyeLookUpLeft,
+        // EyeLookDownLeft,
+
+        #endregion
+
+        #region Eye Expressions
+
         EyeSquintLeft,
         EyeSquintRight,
-        CheekSquintLeft,
-        CheekSquintRight,
-        MouthRaiserUpper,
-        MouthRaiserLower,
-        MouthDimpleLeft,
-        MouthDimpleRight,
-        LipFunnelBottom,
-        LipFunnelBottomLeft,
-        LipFunnelBottomRight,
-        LipFunnelTop,
-        LipFunnelTopRight,
-        LipFunnelTopLeft,
-        MouthPress,
-        MouthPressLeft,
-        MouthPressRight,
-        LipPuckerLeft, // Mouth Pout but not combined
-        LipPuckerRight, // Mouth Pout but not combined
-        MouthStretchLeft,
-        MouthStretchRight,
-        LipSuckTopLeft, // Cheek Suck but not combined
-        LipSuckTopRight, // Cheek Suck but not combined
-        LipSuckBottomLeft, // Cheek Suck but not combined
-        LipSuckBottomRight, // Cheek Suck but not combined        
-        MouthTightener,
-        MouthTightenerLeft,
-        MouthTightenerRight,
-        //MouthClosed,
+        EyeClosedLeft,
+        EyeClosedRight,
+        EyeWideLeft,
+        EyeWideRight,
+        EyeDilationLeft,
+        EyeDilationRight,
+        EyeConstrictLeft,
+        EyeConstrictRight,
+
+        #endregion
+
+        #region EyeBrow Expressions
+        
+        BrowInnerDownLeft,
+        BrowInnerDownRight,
+        BrowOuterDownLeft,
+        BrowOuterDownRight,
+        BrowInnerUpLeft,
+        BrowInnerUpRight,
+        BrowOuterUpLeft,
+        BrowOuterUpRight,
+
+        #endregion
+
+        #region Nose Expressions
+
         NoseSneerLeft,
         NoseSneerRight,
-        //JawDrop, // TESTING MOUTH APE SHAPE CONTROL
-        Max
+
+        #endregion
+
+        #region Cheek Expressions
+
+        CheekSquintLeft,
+        CheekSquintRight,
+        CheekPuffLeft,
+        CheekPuffRight,
+        CheekSuckLeft,
+        CheekSuckRight,
+
+        #endregion
+
+        #region Jaw Exclusive Expressions
+
+        JawOpen,
+        JawLeft,
+        JawRight,
+        JawForward,
+
+        MouthApeShape,
+
+        #endregion
+
+        #region Lip Expressions
+
+        LipSuckTopRight,
+        LipSuckTopLeft,
+        LipSuckBottomRight,
+        LipSuckBottomLeft,
+        LipFunnelTopRight,
+        LipFunnelTopLeft,
+        LipFunnelBottomRight,
+        LipFunnelBottomLeft,
+        LipPuckerLeft,
+        LipPuckerRight,
+        MouthUpperUpLeft,
+        MouthUpperUpRight,
+        MouthLowerDownLeft,
+        MouthLowerDownRight,
+        MouthUpperLeft, //Orig MouthTopLeft
+        MouthUpperRight, //Orig MouthTopRight
+        MouthLowerLeft, //Orig MouthBottomLeft
+        MouthLowerRight, //Orig MouthBottomRight
+        MouthSmileLeft,
+        MouthSmileRight,
+        MouthFrownLeft,
+        MouthFrownRight,
+        MouthDimpleLeft,
+        MouthDimpleRight,
+        MouthRaiserUpper,
+        MouthRaiserLower,
+        // MouthShrugLower, // Duplicate shape to MouthRaiserLower
+        MouthPressLeft,
+        MouthPressRight,
+        MouthTightenerLeft,
+        MouthTightenerRight,
+        MouthStretchLeft,
+        MouthStretchRight,
+
+        #endregion
+
+        #region Tongue Expressions
+
+        TongueOut,
+        TongueDown,
+        TongueUp,
+        TongueLeft,
+        TongueRight,
+        TongueRoll
+
+        #endregion
     }
 }
