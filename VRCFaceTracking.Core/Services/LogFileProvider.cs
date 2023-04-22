b8d0a86eabac1987bc9dbc7ba4421e1f8d33f260
benaclejames <benaclejames@gmail.com>
2023-04-22 09:27:28 +0100

    Eat my ass windows

--- VRCFaceTracking.Core/Services/LogFileProvider.cs
+++ VRCFaceTracking.Core/Services/LogFileProvider.cs
@@ -10,6 +10,9 @@ public class LogFileProvider : ILoggerProvider
     
     public LogFileProvider()
     {
+        if (!Directory.Exists(Utils.PersistentDataDirectory))   // Eat my ass windows
+            Directory.CreateDirectory(Utils.PersistentDataDirectory);
+    
         var logPath = Path.Combine(Utils.PersistentDataDirectory, "latest" + ".log");
         _file = new FileStream(logPath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite, 4096, FileOptions.WriteThrough);
         _writer = new StreamWriter(_file);
