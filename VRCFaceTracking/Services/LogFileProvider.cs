88b029791b6f16e7b1b7dd1224b4b208ae89c498
benaclejames <benaclejames@gmail.com>
2024-04-11 00:56:07 +0100

    fix: Fixed LogFileProvider to not crash on create file fail

--- VRCFaceTracking/Services/LogFileProvider.cs
+++ VRCFaceTracking/Services/LogFileProvider.cs
@@ -1,29 +1,45 @@
 ﻿using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace VRCFaceTracking.Core.Services;
 
 [ProviderAlias("Debug")]
 public class LogFileProvider : ILoggerProvider
 {
-    private readonly FileStream _file;
-    private readonly StreamWriter _writer;
+    private readonly StreamWriter? _writer;
     
     public LogFileProvider()
     {
-        if (!Directory.Exists(Utils.UserAccessibleDataDirectory))   // Eat my ass windows
-            Directory.CreateDirectory(Utils.UserAccessibleDataDirectory);
-    
-        var logPath = Path.Combine(Utils.UserAccessibleDataDirectory, "latest.log");
-        _file = new FileStream(logPath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite, 4096, FileOptions.WriteThrough);
-        _writer = new StreamWriter(_file);
+        try
+        {
+            if (!Directory.Exists(Utils.UserAccessibleDataDirectory)) // Eat my ass windows
+                Directory.CreateDirectory(Utils.UserAccessibleDataDirectory);
+
+            var logPath = Path.Combine(Utils.UserAccessibleDataDirectory, "latest.log");
+
+            var file = new FileStream(logPath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite, 4096,
+                FileOptions.WriteThrough);
+            _writer = new StreamWriter(file);
+        }
+        catch
+        {
+
+        }
     }
     
     private readonly ConcurrentDictionary<string, LogFileLogger> _loggers =
         new(StringComparer.OrdinalIgnoreCase);
 
-    public ILogger CreateLogger(string categoryName) =>
-        _loggers.GetOrAdd(categoryName, name => new LogFileLogger(name, _writer));
+    public ILogger CreateLogger(string categoryName)
+    {
+        if (_writer != null)
+        {
+            return _loggers.GetOrAdd(categoryName, name => new LogFileLogger(name, _writer));
+        }
+
+        return NullLogger.Instance;
+    }
 
     public void Dispose() => _loggers.Clear();
 }
