857f70441ffc0c601b6b8d159b97796704aa6986
Hyblocker <hyblocker@protonmail.com>
2024-06-10 22:24:02 +0200

    (feat): Stub for subprocess IPC

---
+++ VRCFaceTracking.Core/Sandboxing/VrcftSandboxServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRCFaceTracking.Core.Models;
+
+namespace VRCFaceTracking.Core.Sandboxing;
+public class VrcftSandboxServer
+{
+    private static readonly Random Random = new ();
+
+    private string _namedPipeString;
+
+    // View into the sub-process' local data
+    private SubprocessData _subprocessData = new();
+
+    public VrcftSandboxServer(string targetNamedPipe)
+    {
+        _namedPipeString = $"{targetNamedPipe}_moduleId{Random.NextInt64()}";
+    }
+}
