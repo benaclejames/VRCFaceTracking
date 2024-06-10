aa32b4d1302d088d7b6bdac144fbefc01ba1d803
Hyblocker <hyblocker@protonmail.com>
2024-06-10 22:14:21 +0200

    (feat): Implement basic main function

--- VRCFaceTracking.ModuleProcess/ModuleProcessMain.cs
+++ VRCFaceTracking.ModuleProcess/ModuleProcessMain.cs
@@ -4,6 +4,15 @@ public class ModuleProcessMain
 {
     public static int Main(string[] args)
     {
+        if (args.Length < 1)
+        {
+            return ModuleProcessExitCodes.INVALID_ARGS;
+        }
+
+        string namedPipeDestination = string.Join(' ', args);
+
+        // A module process will connect to a given named pipe first. We try connecting to the named pipe for 30 seconds, then give up, returning an error code in the process.
+
         return ModuleProcessExitCodes.OK;
     }
 }
