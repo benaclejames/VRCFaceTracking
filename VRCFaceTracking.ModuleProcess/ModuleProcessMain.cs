8ed9730c51c8658d8dccd215bcb245eb89423cd6
Hyblocker <hyblocker@protonmail.com>
2024-06-11 01:20:30 +0200

    (feat): Slightly more complete sub-process main function.

--- VRCFaceTracking.ModuleProcess/ModuleProcessMain.cs
+++ VRCFaceTracking.ModuleProcess/ModuleProcessMain.cs
@@ -1,4 +1,6 @@
-﻿namespace VRCFaceTracking.ModuleProcess;
+﻿using VRCFaceTracking.Core.Models;
+
+namespace VRCFaceTracking.ModuleProcess;
 
 public class ModuleProcessMain
 {
@@ -6,12 +8,18 @@ public class ModuleProcessMain
     {
         if (args.Length < 1)
         {
+            // Not enough arguments
             return ModuleProcessExitCodes.INVALID_ARGS;
         }
 
         string namedPipeDestination = string.Join(' ', args);
 
         // A module process will connect to a given named pipe first. We try connecting to the named pipe for 30 seconds, then give up, returning an error code in the process.
+        VrcftSandboxClient client = new VrcftSandboxClient(namedPipeDestination);
+        client.Connect();
+
+        SubprocessData subprocessData = new SubprocessData();
+        UnifiedTrackingProxy.UpdateSharedData(ref subprocessData);
 
         return ModuleProcessExitCodes.OK;
     }
