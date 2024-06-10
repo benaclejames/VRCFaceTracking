761ccb7a619d7ca04f58738a0cfa9f117d8c63cd
Hyblocker <hyblocker@protonmail.com>
2024-06-11 01:19:48 +0200

    (feat): Implement barebones named pipe server

--- VRCFaceTracking.Core/Sandboxing/VrcftSandboxServer.cs
+++ VRCFaceTracking.Core/Sandboxing/VrcftSandboxServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,16 @@ public class VrcftSandboxServer
 
     // View into the sub-process' local data
     private SubprocessData _subprocessData = new();
+    private NamedPipeServerStream _namedPipeServer;
 
     public VrcftSandboxServer(string targetNamedPipe)
     {
         _namedPipeString = $"{targetNamedPipe}_moduleId{Random.NextInt64()}";
+        _namedPipeServer = new NamedPipeServerStream(targetNamedPipe, PipeDirection.InOut, 1);
     }
+
+    /// <summary>
+    /// Whether the server is running and connected
+    /// </summary>
+    public bool IsServerRunning => _namedPipeServer?.IsConnected ?? false;
 }
