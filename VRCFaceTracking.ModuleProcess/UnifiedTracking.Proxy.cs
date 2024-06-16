806549926055b1dcbfd248afbd5782a92d52fe5b
Hyblocker <hyblocker@protonmail.com>
2024-06-16 02:29:05 +0200

    (feat): Cleanup UnifiedTracking.Proxy

--- VRCFaceTracking.ModuleProcess/UnifiedTracking.Proxy.cs
+++ VRCFaceTracking.ModuleProcess/UnifiedTracking.Proxy.cs
@@ -14,23 +14,4 @@ namespace VRCFaceTracking.ModuleProcess;
 // Handles syncing our Subprocess data with the "fake" UnifiedTracking implementation we expose to modules
 public static class UnifiedTrackingProxy {
 
-    private static void UpdateImage(ref Image destination, ref Image source)
-    {
-		destination.SupportsImage   = source.SupportsImage;
-		destination.ImageSize       = source.ImageSize;
-		destination.ImageData       = source.ImageData;
-	}
-
-	private static void CopyArrayFast(ref VRCFTParameter[] destination, VRCFTParameter[] source) {
-
-	}
-
-    public static void UpdateSharedData(ref SubprocessData subprocessData)
-    {
-		UpdateImage(ref subprocessData.EyeImageData, ref RealUnifiedTracking.EyeImageData);
-		UpdateImage(ref subprocessData.LipImageData, ref RealUnifiedTracking.LipImageData);
-
-		CopyArrayFast(ref subprocessData.AllParameters_v1, RealUnifiedTracking.AllParameters_v1);
-		CopyArrayFast(ref subprocessData.AllParameters_v2, RealUnifiedTracking.AllParameters_v2);
-    }
 }
