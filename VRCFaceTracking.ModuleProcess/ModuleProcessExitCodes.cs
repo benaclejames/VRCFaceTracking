3274a7012945346d7a0de1753a4be965dc7e8298
Hyblocker <hyblocker@protonmail.com>
2024-06-16 02:28:47 +0200

    (chore): Update exit code enum name

--- VRCFaceTracking.ModuleProcess/ModuleProcessExitCodes.cs
+++ VRCFaceTracking.ModuleProcess/ModuleProcessExitCodes.cs
@@ -3,5 +3,5 @@ public static class ModuleProcessExitCodes
 {
     public const int OK                                         =  0;
     public const int INVALID_ARGS                               = -1;
-    public const int NAMED_PIPE_CONNECTION_TIMED_OUT            = -2;
+    public const int NETWORK_CONNECTION_TIMED_OUT               = -2;
 }
