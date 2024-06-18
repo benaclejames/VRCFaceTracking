6a76fc2e32ac2a51b4c64fafc6f19abd30079840
Hyblocker <hyblocker@protonmail.com>
2024-06-18 18:45:16 +0200

    (feat): Add robust CLI args to the sandbox process

--- VRCFaceTracking.ModuleProcess/ModuleProcessMain.cs
+++ VRCFaceTracking.ModuleProcess/ModuleProcessMain.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Sockets;
+﻿using System.CommandLine;
+using System.Diagnostics;
+using System.Net.Sockets;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using VRCFaceTracking.Core.Models;
@@ -12,20 +14,33 @@ public class ModuleProcessMain
 {
     public static int Main(string[] args)
     {
-        if (args.Length < 1)
+        if ( args.Length < 1 )
         {
             // Not enough arguments
             return ModuleProcessExitCodes.INVALID_ARGS;
         }
 
-        int serverPortNumber = 0;
-        if ( !int.TryParse(args[0], out serverPortNumber) )
+        var portOption = new Option<int?>(
+            name: "--port",
+            description: "The UDP port the VRCFT server is running on.");
+        var modulePathOption = new Option<string?>(
+            name: "--module-path",
+            description: "The path to the module to load.");
+
+        var rootCommand = new RootCommand("VRCFT Sandbox Module");
+        rootCommand.AddOption(portOption);
+        rootCommand.AddOption(modulePathOption);
+
+        rootCommand.SetHandler((modulePath, port) =>
         {
-            // Port number is not a number
-            return ModuleProcessExitCodes.INVALID_ARGS;
-        }
+            VrcftMain(modulePath!, port ?? 0);
+        }, modulePathOption, portOption);
 
+        return rootCommand.Invoke(args);
+    }
 
+    static int VrcftMain(string modulePath, int serverPortNumber)
+    {
         ServiceProvider serviceProvider = new ServiceCollection()
         .AddLogging((loggingBuilder) => loggingBuilder
                 .ClearProviders()
@@ -41,6 +56,8 @@ public class ModuleProcessMain
 
         var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
         var logger = loggerFactory.CreateLogger<ModuleProcessMain>();
+
+        logger.LogInformation($"Module path: {modulePath} ; Got port {serverPortNumber}");
 
         // A module process will connect to a given port number first. We try connecting to the server for 30 seconds, then give up, returning an error code in the process.
         VrcftSandboxClient client = new VrcftSandboxClient(serverPortNumber, loggerFactory);
