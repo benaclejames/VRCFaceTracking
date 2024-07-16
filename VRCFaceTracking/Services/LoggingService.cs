b9a3d49813ffdd698aa4bf410bb2a9416928b5ba
Hyblocker <hyblocker@protonmail.com>
2024-07-16 19:51:04 +0200

    (feat): Make the output page colour log lines based on severity

--- VRCFaceTracking/Services/LoggingService.cs
+++ VRCFaceTracking/Services/LoggingService.cs
@@ -3,12 +3,24 @@ using Microsoft.Extensions.Logging;
 using Microsoft.UI.Dispatching;
 
 namespace VRCFaceTracking.Services;
-    
+
+public struct LogLine
+{
+    public string Message;
+    public LogLevel Level;
+
+    public LogLine(string message, LogLevel level)
+    {
+        Message = message;
+        Level = level;
+    }
+}    
+
 public class OutputPageLogger : ILogger
 {
     private readonly string _categoryName;
-    public static readonly ObservableCollection<string> FilteredLogs = new();
-    public static readonly ObservableCollection<string> AllLogs = new();
+    public static readonly ObservableCollection<LogLine> FilteredLogs = new();
+    public static readonly ObservableCollection<LogLine> AllLogs = new();
     private static DispatcherQueue? _dispatcher;
 
     public OutputPageLogger(string categoryName, DispatcherQueue? queue)
@@ -34,20 +46,20 @@ public class OutputPageLogger : ILogger
             if ( _categoryName == "\0VRCFT\0" )
             {
                 // Log events from sub-processes have the unique category name "\0VRCFT\0"
-                AllLogs.Add($"{formatter(state, exception)}");
+                AllLogs.Add(new LogLine($"{formatter(state, exception)}", logLevel));
                 // Filtered is what the user sees, so show Information scope
                 if ( logLevel >= LogLevel.Information )
                 {
-                    FilteredLogs.Add($"{formatter(state, exception)}");
+                    FilteredLogs.Add(new LogLine($"{formatter(state, exception)}", logLevel));
                 }
             }
             else
             {
-                AllLogs.Add($"[{_categoryName}] {logLevel}: {formatter(state, exception)}");
+                AllLogs.Add(new LogLine($"[{_categoryName}] {logLevel}: {formatter(state, exception)}", logLevel));
                 // Filtered is what the user sees, so show Information scope
                 if ( logLevel >= LogLevel.Information )
                 {
-                    FilteredLogs.Add($"[{_categoryName}] {logLevel}: {formatter(state, exception)}");
+                    FilteredLogs.Add(new LogLine($"[{_categoryName}] {logLevel}: {formatter(state, exception)}", logLevel));
                 }
             }
         });
