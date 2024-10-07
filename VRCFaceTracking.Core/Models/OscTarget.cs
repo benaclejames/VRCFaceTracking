3480ca415c08d0d1e7cdb2a4a8e298beca87663c
Thimo <thimodehaan@gmail.com>
2024-10-07 21:38:54 +0200

    Reverted code structure to original style

--- VRCFaceTracking.Core/Models/OscTarget.cs
+++ VRCFaceTracking.Core/Models/OscTarget.cs
@@ -9,17 +9,17 @@ public partial class OscTarget : ObservableObject, IOscTarget
 {
     [ObservableProperty] private bool _isConnected;
 
-    [ObservableProperty]
-    [property: SavedSetting("OSCInPort", 9001)]
+    [ObservableProperty] [property: SavedSetting("OSCInPort", 9001)]
+
     private int _inPort;
 
-    [ObservableProperty]
-    [property: SavedSetting("OSCOutPort", 9000)]
+    [ObservableProperty] [property: SavedSetting("OSCOutPort", 9000)]
+
     private int _outPort;
 
     [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(InPort))]
-    [NotifyPropertyChangedFor(nameof(OutPort))]
+    [NotifyPropertyChangedFor(nameof(InPort))] [NotifyPropertyChangedFor(nameof(OutPort))]
+
     [property: SavedSetting("OSCAddress", "127.0.0.1")]
     private string _destinationAddress;
 
