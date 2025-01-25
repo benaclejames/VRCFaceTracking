85b77e5cc720c5f9570509adc2b10c697dcc24b5
ksmg <kusomaigo@gmail.com>
2025-01-25 00:33:02 -0800

    clarification to head orientation summary

--- VRCFaceTracking.Core/Params/Data/UnifiedData.cs
+++ VRCFaceTracking.Core/Params/Data/UnifiedData.cs
@@ -87,10 +87,13 @@ namespace VRCFaceTracking.Core.Params.Data
     {
         /// <summary>
         /// Contains head rotation and position data.
+        /// Head coordinate system should be *LEFT handed, Y-up* (matching Unity coordinate system)
         /// Head rotation values should be normalized to a [-1, 1] range representing -90d to 90d rotation (0 meaning facing directly forwards)
+        ///     Yaw - Y axis rotation (positive values = looking right)
+        ///     Pitch - X axis rotation (positive values = looking down)
+        ///     Roll - Z axis rotation (positive values = side-tilt left)
         /// Head position values should be normalized / capped to a [-1, 1] range and represent deviation from a set user origin point in meters
         ///     The normalized values should be *approximately* represent a 1x1x1 meter movement region about the user's origin point 
-        ///     Head position coordinates should be *LEFT handed, Y-up* (matching Unity coordinate system)
         ///     (i.e. HeadPosX = 1 means the user is 0.5m to their right from their starting point, HeadPosX = -1 the user is 0.5m to their left)
         /// </summary>
         public float HeadYaw;
