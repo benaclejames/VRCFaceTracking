a5f7150d555d588fc47f4ceaaa429f8245c597d6
regzo2 <regzo252@gmail.com>
2025-02-13 15:42:40 -0500

    feat(ui): Added categorized Error level logs to the Home tab.

---
+++ VRCFaceTracking/Models/LoggingContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VRCFaceTracking.Models;
+
+public class LoggingContext : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    private bool _visible = true;
+    public bool Visible 
+    { 
+        get => _visible;
+        set
+        {
+            _visible = value;
+            NotifyPropertyChanged();
+        } 
+    }
+
+    private string _context;
+    public string Context 
+    { 
+        get => _context;
+        set
+        {
+            _context = value;
+            NotifyPropertyChanged();
+        } 
+    }
+    private string _logs;
+
+    public string Logs 
+    { 
+        get => _logs;
+        set
+        {
+            _logs = value;
+            NotifyPropertyChanged();
+        } 
+    }
+}
--- VRCFaceTracking/Services/LoggingService.cs
+++ VRCFaceTracking/Services/LoggingService.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections.ObjectModel;
+﻿using VRCFaceTracking.Models;
+using System.Collections.ObjectModel;
 using Microsoft.Extensions.Logging;
 using Microsoft.UI.Dispatching;
 
 namespace VRCFaceTracking.Services;
-    
+
 public class OutputPageLogger : ILogger
 {
     private readonly string _categoryName;
     public static readonly ObservableCollection<string> FilteredLogs = new();
     public static readonly ObservableCollection<string> AllLogs = new();
+    public static ObservableCollection<LoggingContext> ErrorLogs = new();
     private static DispatcherQueue? _dispatcher;
 
     public OutputPageLogger(string categoryName, DispatcherQueue? queue)
@@ -28,14 +30,35 @@ public class OutputPageLogger : ILogger
         Exception? exception,
         Func<TState, Exception?, string> formatter)
     {
-        // Add to the staticLog from the dispatcher thread
         _dispatcher?.TryEnqueue(() =>
         {
-            AllLogs.Add($"[{_categoryName}] {logLevel}: {formatter(state, exception)}");
+            // Add to the staticLog from the dispatcher thread
+            var log = $"[{_categoryName}] {logLevel}: {formatter(state, exception)}";
+            AllLogs.Add(log);
             // Filtered is what the user sees, so show Information scope
             if (logLevel >= LogLevel.Information)
             {
-                FilteredLogs.Add($"[{_categoryName}] {logLevel}: {formatter(state, exception)}");
+                FilteredLogs.Add(log);
+            }
+
+            if (logLevel == LogLevel.Error)
+            {
+                var skimmedLog = $"{formatter(state, exception)}";
+
+                // Find the matching log context
+                var existingLog = ErrorLogs.FirstOrDefault(item => item.Context == _categoryName);
+
+                if (existingLog != null)
+                {
+                    if (!existingLog.Logs.Contains(skimmedLog))
+                    {
+                        existingLog.Logs += $"\n{skimmedLog}";
+                    }
+                }
+                else
+                {
+                    ErrorLogs.Add(new LoggingContext { Context = _categoryName, Logs = skimmedLog });
+                }
             }
         });
     }
