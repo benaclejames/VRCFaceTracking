da6c413af31767307a556ffc6a5270ed94fa03cc
Ben Thomas <benaclejames@pm.me>
2025-08-10 14:27:54 +0100

    refactor: Code cleanup. Upgrade c# language version to latest

--- VRCFaceTracking.Core/mDNS/MulticastDnsService.cs
+++ VRCFaceTracking.Core/mDNS/MulticastDnsService.cs
@@ -21,7 +21,7 @@ public partial class MulticastDnsService : ObservableObject
 
     public Action OnVrcClientDiscovered = () => { };
 
-    [ObservableProperty] private IPEndPoint? _vrchatClientEndpoint;
+    [ObservableProperty] private IPEndPoint _vrchatClientEndpoint;
 
     private static List<NetworkInterface> GetIpv4NetInterfaces() => NetworkInterface.GetAllNetworkInterfaces()
         .Where(net =>
