56e684959f9b23ef309a10eeb47a723058ce587c
benaclejames <benaclejames@gmail.com>
2026-08-29 21:35:17 +0100

    Better conditionals for native=. Cleanup native definitions

--- VRCFaceTracking.Core/Services/BinaryFaceDataSender.cs
+++ VRCFaceTracking.Core/Services/BinaryFaceDataSender.cs
@@ -1,10 +1,11 @@
 using Microsoft.Extensions.Hosting;
 using VRCFaceTracking.Core.OSC;
+using VRCFaceTracking.Core.Params;
 using VRCFaceTracking.Core.Params.Data;
 
 namespace VRCFaceTracking.Core.Services;
 
-public class BinaryFaceDataSender : IHostedService
+public class BinaryFaceDataSender(OscQueryService oscqService) : IHostedService
 {
     private byte[] _blobBuffer = new byte[UnifiedTrackingData.SerializedLength];
     private OscMessage? _blobMessage;
@@ -12,7 +13,13 @@ public class BinaryFaceDataSender : IHostedService
     public Task StartAsync(CancellationToken cancellationToken)
     {
         _blobMessage = OscMessage.CreateBlob("/tracking/face/v1", _blobBuffer, _blobBuffer.Length);
+        
         UnifiedTracking.OnUnifiedDataUpdated += OnDataUpdated;
+        oscqService.PropertyChanged += (sender, args) =>
+        {
+            if (args.PropertyName != nameof(OscQueryService.AvatarParameters)) return;
+            SubscribeConditional((sender as OscQueryService)!.AvatarParameters);
+        };
         return Task.CompletedTask;
     }
 
@@ -22,6 +29,21 @@ public class BinaryFaceDataSender : IHostedService
         return Task.CompletedTask;
     }
 
+    private void SubscribeConditional(IEnumerable<Parameter> parameters)
+    {
+        var shouldBeSubscribed = !parameters.Any();
+        var isAlreadySubscribed = UnifiedTracking.OnUnifiedDataUpdated.GetInvocationList().Any(x => x.Target == this);
+        
+        if (shouldBeSubscribed && !isAlreadySubscribed)
+        {
+            UnifiedTracking.OnUnifiedDataUpdated += OnDataUpdated;
+        }
+        else if (!shouldBeSubscribed && isAlreadySubscribed)
+        {
+            UnifiedTracking.OnUnifiedDataUpdated -= OnDataUpdated;
+        }
+    }
+    
     private void OnDataUpdated(UnifiedTrackingData data)
     {
         if (_blobMessage == null) return;
